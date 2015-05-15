8a5ad0d7fdb0ed370f6d957e8dd2e288bf6a3e90
pokutuna <popopopopokutuna@gmail.com>
2015-05-15 09:13:48 +0900

    fetch

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,18 @@
-console.log('hi');
+/// <reference path="../../modules/DefinitelyTyped/chrome/chrome.d.ts" />
+
+module TabLiver {
+
+    export function init(): void {
+        var tabFetcher = function() {
+            setTimeout(() => {
+                chrome.tabs.getCurrent( (tab: chrome.tabs.Tab) => {
+                    console.log(tab.id, tab.url);
+                    tabFetcher();
+                });
+            }, 300);
+        };
+        tabFetcher();
+    }
+}
+
+TabLiver.init();
