888db2f0d98a496156a1470c8c9185c7b6b5f951
pokutuna <popopopopokutuna@gmail.com>
2015-05-19 18:30:08 +0900

    warn

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp       = require('gulp'),
+    gutil      = require('gulp-util'),
     editJson   = require('gulp-json-editor'),
     typescript = require('gulp-typescript'),
     sass       = require('gulp-sass'),
@@ -46,6 +47,10 @@ function version() {
         });
     });
     return promise.then(function(desc) {
+        if (!/\d.\d.\d/.test(desc)) {
+            gutil.log(gutil.colors.yellow('add git tag 1-3 dot-separated intergers (e.g. 0.0.1) for auto-versioning'));
+            desc = '0.0.0.0';
+        }
         var version = desc.replace(/\n$/, '')
             .replace(/-(\d+)/, '.$1')
             .replace(/-g[0-9a-f]+/, '')
