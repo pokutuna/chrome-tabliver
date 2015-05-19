86c492bc40e3df21247437eb9d21d1bcd6a21d8d
pokutuna <popopopopokutuna@gmail.com>
2015-05-19 19:05:58 +0900

    fix

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,10 +3,14 @@
 module TabLiver {
 
     export function init(): void {
+        var query: chrome.tabs.QueryInfo = {
+            active: true,
+            currentWindow: true,
+        };
         var tabFetcher = function() {
             setTimeout(() => {
-                chrome.tabs.getCurrent( (tab: chrome.tabs.Tab) => {
-                    console.log(tab.id, tab.url);
+                chrome.tabs.query(query, (tabs: chrome.tabs.Tab[]) => {
+                    if (tabs.length) console.log(tabs[0].id, tabs[0].url);
                     tabFetcher();
                 });
             }, 300);
